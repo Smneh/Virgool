1dd2d8e44ed42f4438f8d53020560c8eb82decf6
Smneh <a.smneh79@gmail.com>
2022-04-22 18:44:57 +0430

    add new styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,16 +12,43 @@ export default {
 }
 </script>
 
-<style >
+<style lang="scss">
 @import url('https://v1.fontapi.ir/css/Vazir');
 
-.v-application {
-  font-family: Vazirmatn, sans-serif !important;
+.theme--light {
+  &.v-application {
+    font-family: Vazirmatn, sans-serif !important;
+    line-height: 2 !important;
+    direction: rtl;
+    word-wrap: break-word;
+    background-color: $white;
+    color: $text-color;
+    font-size: 16px;
+    font-weight: 400;
+    letter-spacing: 0;
+  }
 }
 
 .content-container {
-    max-width: 1000px !important;
-    padding-left: 16px !important;
-    padding-right: 16px !important;
+  width: 100%;
+  margin: 0 auto;
+  max-width: 1000px !important;
+  padding: 0 16px !important;
 }
+
+.section-subtitle{
+    color: $light-gray;
+    font-family: inherit;
+    font-size: 12.8px;
+}
+
+.section-body {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.row{
+  margin: 0 !important;
+}
+
 </style>
